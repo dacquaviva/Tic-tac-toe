7070279a48dcac4736a88b4137943a6d1aaef1cd
Daniele Acquaviva <danieleacquaviva5@gmail.com>
2018-11-03 23:17:05 +0100

    Added function to get the next symbol

--- scripts/game.js
+++ scripts/game.js
@@ -2,12 +2,14 @@ function Game(numRow,numCol) {
   let board = [];
   let row = numRow;
   let col = numCol;
+  let symbol;
+  let numSymbol = 0;
   this.initializeBoard = function(){
     for(var i=0; i<row; i++) {
       board[i] = new Array(col).fill(" ");
     }
   };
-  
+
   this.cleanBoard = function(){
     let cells = document.getElementsByTagName('button');
     for(var i=0; i<cells.length; i++) {
@@ -15,13 +17,31 @@ function Game(numRow,numCol) {
       cells[i].innerHTML = " ";
     }
   };
+
+  this.getSymbol = function(){
+    nextSymbol();
+    return symbol;
+  };
+
+  let nextSymbol = function(){
+    if(numSymbol % 2 === 0){
+        numSymbol++;
+        symbol = "X"
+    }else {
+      numSymbol++;
+      symbol = "O"
+    }
+  };
+
 }
 
 
 const  COL = 3;
 const  ROW = 3;
 const game = new Game(ROW,COL);
+let symbol = " ";
 game.initializeBoard();
 function play(position){
-  game.cleanBoard();
+  symbol = game.getSymbol();
+  alert(symbol);
 }
